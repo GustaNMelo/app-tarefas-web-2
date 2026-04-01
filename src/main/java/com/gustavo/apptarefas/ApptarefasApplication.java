599851b68c3437df04d1ee8f951c25a77ed87d97
Gustavo Nunes <gustavonunesmelo@outlook.com>
2026-03-31 21:16:46 -0300

    Primeiro commit

---
+++ src/main/java/com/gustavo/apptarefas/ApptarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.apptarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptarefasApplication.class, args);
+	}
+
+}
